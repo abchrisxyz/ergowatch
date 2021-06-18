9a19a81d6d808b9b25aa6f64c618e6fb4baba834
chris <abc@abchris.xyz>
2021-06-19 08:02:21 +1200

    Fix build script

--- build.sh
+++ build.sh
@@ -9,12 +9,11 @@ curl -L https://github.com/ergoplatform/explorer-backend/archive/refs/tags/$EXPL
 
 echo "Extracting explorer source"
 rm -rf explorer-backend/$EXPLORER_VERSION
-mkdir explorer-backend/$EXPLORER_VERSION
 tar -xf explorer-backend-$EXPLORER_VERSION.tar.gz
-mv explorer-backend-$EXPLORER_VERSION explorer-backend/$EXPLORER_VERSION/explorer-backend
+mv explorer-backend-$EXPLORER_VERSION explorer-backend/$EXPLORER_VERSION
 rm explorer-backend-$EXPLORER_VERSION.tar.gz
 
 echo "Preparing explorer Dockerfile"
-cp explorer-backend/$EXPLORER_VERSION/explorer-backend/modules/chain-grabber/Dockerfile explorer-backend/$EXPLORER_VERSION/chain-grabber.Dockerfile
+cp explorer-backend/$EXPLORER_VERSION/modules/chain-grabber/Dockerfile explorer-backend/$EXPLORER_VERSION/chain-grabber.Dockerfile
 
 echo "Done."
