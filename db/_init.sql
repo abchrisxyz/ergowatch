1ef6c5bdbfee037accaf79f64c25d72cb51b8183
chris <abc@abchris.xyz>
2021-12-03 09:57:16 +1300

    Update db init script

--- db/_init.sql
+++ db/_init.sql
@@ -1,5 +1,5 @@
 /*
-	psql input file to load schema's right order to satisfy dependencies.
+	psql input file to load schema's in right order to satisfy dependencies.
 
 	Example:
 
@@ -8,6 +8,6 @@
 \i './oracle-pools.sql'
 \i './sigmausd.sql'
 \i './coingecko.sql'
-\i './age.sql'
-\i './distribution.sql'
+\i './snapshots.sql'
+\i './continuous.sql'
 \i './sync.sql'
