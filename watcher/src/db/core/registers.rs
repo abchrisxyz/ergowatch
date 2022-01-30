8110081f8640e3b284c17b0a5161579fb3bab291
chris <abc@abchris.xyz>
2022-01-31 12:41:12 +1300

    Fix core insert box registers statement

--- watcher/src/db/core/registers.rs
+++ watcher/src/db/core/registers.rs
@@ -2,8 +2,8 @@ use crate::db::SQLArg;
 use crate::db::SQLStatement;
 
 pub const INSERT_BOX_REGISTER: &str = "\
-    insert into core.box_registers (id, box_id, stype, serialized_value, rendered_value) \
-    values ($1, $2, $3, $4 &5);";
+    insert into core.box_registers (id, box_id, value_type, serialized_value, rendered_value) \
+    values ($1, $2, $3, $4, $5);";
 
 pub struct BoxRegisterRow<'a> {
     pub id: i16,
