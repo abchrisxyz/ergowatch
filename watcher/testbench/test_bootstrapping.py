fab82f55dfeca453663e8ba7ee46469559bce807
chris <abc@abchris.xyz>
2022-02-17 21:02:07 +1300

    Test bootstrap mode sets database constraints after sync

--- watcher/testbench/test_bootstrapping.py
+++ watcher/testbench/test_bootstrapping.py
@@ -6,6 +6,29 @@ from fixtures import unconstrained_db_env
 from utils import run_watcher
 
 
+def test_constraints_are_set_after_bootstrapping(unconstrained_db_env):
+    """
+    When done syncing, bootstrapping mode should load database constraints.
+    """
+    db_conn, cfg_path = unconstrained_db_env
+
+    # No constraints before run
+    with db_conn.cursor() as cur:
+        cur.execute("select constraints_set from ew.revision;")
+        row = cur.fetchone()
+    assert row[0] == False
+
+    # Run
+    cp = run_watcher(cfg_path, bootstrap=True, sync_only=False)
+    assert cp.returncode == 0
+
+    # Constrains after run
+    with db_conn.cursor() as cur:
+        cur.execute("select constraints_set from ew.revision;")
+        row = cur.fetchone()
+    assert row[0] == True
+
+
 def test_constraints_set_constraints_flag(fork_env):
     """
     Test db relevant db flag is set after applying constraints.
