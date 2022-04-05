a347440684b04d669fde5e76d482cff9cbfc6a7d
chris <abc@abchris.xyz>
2022-04-05 14:37:05 +1200

    Update docs

--- api/src/main.py
+++ api/src/main.py
@@ -31,11 +31,30 @@ Rules of thumb:
  - all token values expressed in integer form
  - all timestamps in milliseconds since unix epoch (same as node api)
  
-Most API's will accept a `?token_id=` query parameter to return data relating to a given token instead of ERG itself.
+Most endpoints will accept a `?token_id=` query parameter to return data relating to a given token instead of ERG itself.
 
 [Swagger]({root_path}/docs) | [ReDoc]({root_path}/redoc)
 
 [Release notes](https://github.com/abchrisxyz/ergowatch/blob/master/CHANGELOG.md)
+
+### Time windows
+Endpoints under `/metrics` take the following query parameters to define a time window and its resolution:
+- `fr`: first timestamp of time window
+- `to`: last timestamp of time window
+- `r`: resolution of time window, one of `block`, `1h` or `24h`
+
+The distance between `fr` and `to` should not exceed 1000 times the resolution size, allowing for the following time windows:
+- `block`: 120,000 ms * 1000 (~1.4 days, assuming 120 second blocks)
+- `1h`: 3,600,000 ms * 1000 (~41 days)
+- `24h`: 86,400,000 ms * 1000 (~2.7 years)
+
+If `r` is omitted, default `block` level is used.
+
+If none of `fr` and `to` are specified, returns last `24h`, `1h` or `block` level record, according to `r`.
+
+If `fr` is specified without `to`, returns records since `fr`, up to max of window size.
+
+If `to` is specified without `fr`, returns records prior to (and including) `to`, up to max of window size.
 """
 # TODO: explain history vs series
 
@@ -56,6 +75,7 @@ tags_metadata = [
         "name": "contracts",
         "description": "P2S & P2SH address stats",
     },
+    {"name": "metrics", "description": "Metrics over time"},
 ]
 
 app = FastAPI(
