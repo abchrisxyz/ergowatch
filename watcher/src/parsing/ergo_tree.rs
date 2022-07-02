4704462af910390116f85f93f02331f236dfa165
chris <abc@abchris.xyz>
2022-07-02 15:20:41 +1200

    Add unit test variant

--- watcher/src/parsing/ergo_tree.rs
+++ watcher/src/parsing/ergo_tree.rs
@@ -17,13 +17,24 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn check_address_from_ergo_tree() {
+    fn check_address_from_ergo_tree_p2pk() {
         let base16_str = "0008cd03553448c194fdd843c87d080f5e8ed983f5bb2807b13b45a9683bba8c7bfb5ae8";
         let tree_bytes = base16::decode(base16_str.as_bytes()).unwrap();
         let tree = ErgoTree::sigma_parse_bytes(&tree_bytes).unwrap();
         assert_eq!(
             address_from_ergo_tree(&tree),
             "9h7L7sUHZk43VQC3PHtSp5ujAWcZtYmWATBH746wi75C5XHi68b"
+        );
+    }
+
+    #[test]
+    fn check_address_from_ergo_tree_p2s() {
+        let base16_str = "100204a00b08cd033b2ee29e9a4f9e337bf1960015a34e56d9cef041c5fb89ec44f2412ba1cd1689ea02d192a39a8cc7a70173007301";
+        let tree_bytes = base16::decode(base16_str.as_bytes()).unwrap();
+        let tree = ErgoTree::sigma_parse_bytes(&tree_bytes).unwrap();
+        assert_eq!(
+            address_from_ergo_tree(&tree),
+            "88dhgzEuTXaTr9yGAQawohWXzEkk7bESXNuSyrC3F7xNFDq6z4S9RoefjjzTSEoHc1GnxXSE8zngaE7m"
         );
     }
 }
