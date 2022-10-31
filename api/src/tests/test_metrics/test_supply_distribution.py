c3f9733417c0539a6dc1b2f19150f4e4b09d6e40
chris <abc@abchris.xyz>
2022-11-01 12:07:12 +1300

    Refactor and parameterise supply distribution api tests

--- api/src/tests/test_metrics/test_supply_distribution.py
+++ api/src/tests/test_metrics/test_supply_distribution.py
@@ -126,89 +126,17 @@ def base_url(address_type: str, r: str):
     return f"/metrics/supply/distribution/{address_type}?r={r}"
 
 
-class TestCountBlock:
-    r = "block"
-
-    def test_default(self, client):
-        url = base_url(client.address_type, self.r)
-        response = client.get(url)
-        assert response.status_code == 200
-        # Return last block record
-        assert response.json() == {
-            "timestamps": [LAST_TS],
-            "top_1prc": [LAST_VAL[0]],
-            "top_1k": [LAST_VAL[1]],
-            "top_100": [LAST_VAL[2]],
-            "top_10": [LAST_VAL[3]],
-            "circ_supply": [LAST_SC],
-        }
-
-    def test_from_to(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={BLOCK_TSS[1]}&to={BLOCK_TSS[3]}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": BLOCK_TSS[1:4],
-            "top_1prc": [row[0] for row in BLOCK_VALS[1:4]],
-            "top_1k": [row[1] for row in BLOCK_VALS[1:4]],
-            "top_100": [row[2] for row in BLOCK_VALS[1:4]],
-            "top_10": [row[3] for row in BLOCK_VALS[1:4]],
-            "circ_supply": BLOCK_SCS[1:4],
-        }
-
-    def test_from_only(self, client):
-        # from height 1
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={BLOCK_TSS[1]}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": BLOCK_TSS[1:5],
-            "top_1prc": [row[0] for row in BLOCK_VALS[1:5]],
-            "top_1k": [row[1] for row in BLOCK_VALS[1:5]],
-            "top_100": [row[2] for row in BLOCK_VALS[1:5]],
-            "top_10": [row[3] for row in BLOCK_VALS[1:5]],
-            "circ_supply": BLOCK_SCS[1:5],
-        }
-
-    def test_to_only(self, client):
-        # to right after block 5999
-        url = base_url(client.address_type, self.r)
-        url += f"&to={BLOCK_TSS[4] + 10}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": BLOCK_TSS[2:5],
-            "top_1prc": [row[0] for row in BLOCK_VALS[2:5]],
-            "top_1k": [row[1] for row in BLOCK_VALS[2:5]],
-            "top_100": [row[2] for row in BLOCK_VALS[2:5]],
-            "top_10": [row[3] for row in BLOCK_VALS[2:5]],
-            "circ_supply": BLOCK_SCS[2:5],
-        }
-
-    def test_from_prior_to_genesis(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={GENESIS_TIMESTAMP - 1}"
-        response = client.get(url)
-        assert response.status_code == 422
-
-    def test_window_size(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={BLOCK_TSS[0]}&to={BLOCK_TSS[5]}"
-        response = client.get(url)
-        assert response.status_code == 422
-        assert (
-            response.json()["detail"]
-            == f"Time window is limited to {120000 * LIMIT} for block resolution"
-        )
-
-
-class TestCountHourly:
-    r = "1h"
-
-    def test_default(self, client):
-        url = base_url(client.address_type, self.r)
+@pytest.mark.parametrize(
+    "r,dt,tss,vals,scs",
+    [
+        ("block", BLOCK_DT, BLOCK_TSS, BLOCK_VALS, BLOCK_SCS),
+        ("1h", HOUR_DT, HOUR_TSS, HOUR_VALS, HOUR_SCS),
+        ("24h", DAY_DT, DAY_TSS, DAY_VALS, DAY_SCS),
+    ],
+)
+class TestSeriesApi:
+    def test_default(self, client, r, dt, tss, vals, scs):
+        url = base_url(client.address_type, r)
         response = client.get(url)
         assert response.status_code == 200
         # Return latest record
@@ -221,81 +149,81 @@ class TestCountHourly:
             "circ_supply": [LAST_SC],
         }
 
-    def test_from_to(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={HOUR_TSS[1] - 1}&to={HOUR_TSS[3] + 1}"
+    def test_from_to(self, client, r, dt, tss, vals, scs):
+        url = base_url(client.address_type, r)
+        url += f"&fr={tss[1] - 1}&to={tss[3] + 1}"
         response = client.get(url)
         assert response.status_code == 200
         assert response.json() == {
-            "timestamps": HOUR_TSS[1:4],
-            "top_1prc": [row[0] for row in HOUR_VALS[1:4]],
-            "top_1k": [row[1] for row in HOUR_VALS[1:4]],
-            "top_100": [row[2] for row in HOUR_VALS[1:4]],
-            "top_10": [row[3] for row in HOUR_VALS[1:4]],
-            "circ_supply": HOUR_SCS[1:4],
+            "timestamps": tss[1:4],
+            "top_1prc": [row[0] for row in vals[1:4]],
+            "top_1k": [row[1] for row in vals[1:4]],
+            "top_100": [row[2] for row in vals[1:4]],
+            "top_10": [row[3] for row in vals[1:4]],
+            "circ_supply": scs[1:4],
         }
 
-    def test_from_to_window_limit(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={HOUR_TSS[1]}&to={HOUR_TSS[5]}"
+    def test_from_to_window_limit(self, client, r, dt, tss, vals, scs):
+        url = base_url(client.address_type, r)
+        url += f"&fr={tss[1]}&to={tss[5]}"
         response = client.get(url)
         assert response.status_code == 422
         assert (
             response.json()["detail"]
-            == f"Time window is limited to {LIMIT * HOUR_DT} for 1h resolution"
+            == f"Time window is limited to {LIMIT * dt} for {r} resolution"
         )
 
-    def test_from_just_before_new(self, client):
+    def test_from_just_before_new(self, client, r, dt, tss, vals, scs):
         # from timestamp between h1 and h2, just before h2
         # expect value at h2, h3 and h4
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={HOUR_TSS[1] - 100}"
+        url = base_url(client.address_type, r)
+        url += f"&fr={tss[1] - 100}"
         response = client.get(url)
         assert response.status_code == 200
         assert response.json() == {
-            "timestamps": HOUR_TSS[1:4],
-            "top_1prc": [row[0] for row in HOUR_VALS[1:4]],
-            "top_1k": [row[1] for row in HOUR_VALS[1:4]],
-            "top_100": [row[2] for row in HOUR_VALS[1:4]],
-            "top_10": [row[3] for row in HOUR_VALS[1:4]],
-            "circ_supply": HOUR_SCS[1:4],
+            "timestamps": tss[1:4],
+            "top_1prc": [row[0] for row in vals[1:4]],
+            "top_1k": [row[1] for row in vals[1:4]],
+            "top_100": [row[2] for row in vals[1:4]],
+            "top_10": [row[3] for row in vals[1:4]],
+            "circ_supply": scs[1:4],
         }
 
-    def test_from_spot(self, client):
+    def test_from_spot(self, client, r, dt, tss, vals, scs):
         # from timestamp at h2
         # expect value at h2, h3, h4 and h5
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={HOUR_TSS[1]}"
+        url = base_url(client.address_type, r)
+        url += f"&fr={tss[1]}"
         response = client.get(url)
         assert response.status_code == 200
         assert response.json() == {
-            "timestamps": HOUR_TSS[1:5],
-            "top_1prc": [row[0] for row in HOUR_VALS[1:5]],
-            "top_1k": [row[1] for row in HOUR_VALS[1:5]],
-            "top_100": [row[2] for row in HOUR_VALS[1:5]],
-            "top_10": [row[3] for row in HOUR_VALS[1:5]],
-            "circ_supply": HOUR_SCS[1:5],
+            "timestamps": tss[1:5],
+            "top_1prc": [row[0] for row in vals[1:5]],
+            "top_1k": [row[1] for row in vals[1:5]],
+            "top_100": [row[2] for row in vals[1:5]],
+            "top_10": [row[3] for row in vals[1:5]],
+            "circ_supply": scs[1:5],
         }
 
-    def test_from_just_after_spot(self, client):
+    def test_from_just_after_spot(self, client, r, dt, tss, vals, scs):
         # from timestamp just after h4
         # expect value at h5 and h6
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={HOUR_TSS[3] + 1}"
+        url = base_url(client.address_type, r)
+        url += f"&fr={tss[3] + 1}"
         response = client.get(url)
         assert response.status_code == 200
         assert response.json() == {
-            "timestamps": HOUR_TSS[4:6],
-            "top_1prc": [row[0] for row in HOUR_VALS[4:6]],
-            "top_1k": [row[1] for row in HOUR_VALS[4:6]],
-            "top_100": [row[2] for row in HOUR_VALS[4:6]],
-            "top_10": [row[3] for row in HOUR_VALS[4:6]],
-            "circ_supply": HOUR_SCS[4:6],
+            "timestamps": tss[4:6],
+            "top_1prc": [row[0] for row in vals[4:6]],
+            "top_1k": [row[1] for row in vals[4:6]],
+            "top_100": [row[2] for row in vals[4:6]],
+            "top_10": [row[3] for row in vals[4:6]],
+            "circ_supply": scs[4:6],
         }
 
-    def test_out_of_range(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={HOUR_TSS[-1] + 10}"
+    def test_out_of_range(self, client, r, dt, tss, vals, scs):
+        url = base_url(client.address_type, r)
+        url += f"&fr={tss[-1] + 10}"
         response = client.get(url)
         assert response.status_code == 200
         assert response.json() == {
@@ -307,202 +235,49 @@ class TestCountHourly:
             "circ_supply": [],
         }
 
-    def test_to_only(self, client):
+    def test_to_only(self, client, r, dt, tss, vals, scs):
         # to timestamp of block h4
         # expect values at h2, h3 and h4
-        url = base_url(client.address_type, self.r)
-        url += f"&to={HOUR_TSS[3] + 100000}"
+        url = base_url(client.address_type, r)
+        url += f"&to={tss[3] + 100000}"
         response = client.get(url)
         assert response.status_code == 200
         assert response.json() == {
-            "timestamps": HOUR_TSS[1:4],
-            "top_1prc": [row[0] for row in HOUR_VALS[1:4]],
-            "top_1k": [row[1] for row in HOUR_VALS[1:4]],
-            "top_100": [row[2] for row in HOUR_VALS[1:4]],
-            "top_10": [row[3] for row in HOUR_VALS[1:4]],
-            "circ_supply": HOUR_SCS[1:4],
+            "timestamps": tss[1:4],
+            "top_1prc": [row[0] for row in vals[1:4]],
+            "top_1k": [row[1] for row in vals[1:4]],
+            "top_100": [row[2] for row in vals[1:4]],
+            "top_10": [row[3] for row in vals[1:4]],
+            "circ_supply": scs[1:4],
         }
 
-    def test_from_prior_to_genesis(self, client):
-        url = base_url(client.address_type, self.r)
+    def test_from_prior_to_genesis(self, client, r, dt, tss, vals, scs):
+        url = base_url(client.address_type, r)
         url += f"&fr={GENESIS_TIMESTAMP - 1}"
         response = client.get(url)
         assert response.status_code == 422
 
-    def test_from_ge_to(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={HOUR_TSS[3]}&to={HOUR_TSS[3] - 1}"
+    def test_from_ge_to(self, client, r, dt, tss, vals, scs):
+        url = base_url(client.address_type, r)
+        url += f"&fr={tss[3]}&to={tss[3] - 1}"
         response = client.get(url)
         assert response.status_code == 422
         assert response.json()["detail"] == "Parameter `fr` cannot be higher than `to`"
 
-    def test_to_gt_sync_height(self, client):
+    def test_to_gt_sync_height(self, client, r, dt, tss, vals, scs):
         # to 2 hours after last one
         # expect value at h5 and h6
-        url = base_url(client.address_type, self.r)
-        url += f"&to={HOUR_TSS[-1] + HOUR_DT * 2}"
+        url = base_url(client.address_type, r)
+        url += f"&to={tss[-1] + dt * 2}"
         response = client.get(url)
         assert response.status_code == 200
         assert response.json() == {
-            "timestamps": HOUR_TSS[4:6],
-            "top_1prc": [row[0] for row in HOUR_VALS[4:6]],
-            "top_1k": [row[1] for row in HOUR_VALS[4:6]],
-            "top_100": [row[2] for row in HOUR_VALS[4:6]],
-            "top_10": [row[3] for row in HOUR_VALS[4:6]],
-            "circ_supply": HOUR_SCS[4:6],
-        }
-
-
-class TestCountDaily:
-    r = "24h"
-
-    def test_default(self, client):
-        url = base_url(client.address_type, self.r)
-        response = client.get(url)
-        assert response.status_code == 200
-        # Return latest record
-        assert response.json() == {
-            "timestamps": [LAST_TS],
-            "top_1prc": [LAST_VAL[0]],
-            "top_1k": [LAST_VAL[1]],
-            "top_100": [LAST_VAL[2]],
-            "top_10": [LAST_VAL[3]],
-            "circ_supply": [LAST_SC],
-        }
-
-    def test_from_to(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={DAY_TSS[1] - 1}&to={DAY_TSS[3] + 1}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": DAY_TSS[1:4],
-            "top_1prc": [row[0] for row in DAY_VALS[1:4]],
-            "top_1k": [row[1] for row in DAY_VALS[1:4]],
-            "top_100": [row[2] for row in DAY_VALS[1:4]],
-            "top_10": [row[3] for row in DAY_VALS[1:4]],
-            "circ_supply": DAY_SCS[1:4],
-        }
-
-    def test_from_to_window_limit(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={DAY_TSS[1]}&to={DAY_TSS[5]}"
-        response = client.get(url)
-        assert response.status_code == 422
-        assert (
-            response.json()["detail"]
-            == f"Time window is limited to {LIMIT * DAY_DT} for 24h resolution"
-        )
-
-    def test_from_just_before_new(self, client):
-        # from timestamp between d1 and d2, just before d2
-        # expect value at d2, d3 and d4
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={1562112000000 - 100}"
-        # from timestamp between d1 and d2, just before d2
-        # expect value at d2, d3 and d4
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={DAY_TSS[1] - 100}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": DAY_TSS[1:4],
-            "top_1prc": [row[0] for row in DAY_VALS[1:4]],
-            "top_1k": [row[1] for row in DAY_VALS[1:4]],
-            "top_100": [row[2] for row in DAY_VALS[1:4]],
-            "top_10": [row[3] for row in DAY_VALS[1:4]],
-            "circ_supply": DAY_SCS[1:4],
-        }
-
-    def test_from_spot(self, client):
-        # from timestamp at d2
-        # expect value at d2, d3, d3 and d5
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={DAY_TSS[1]}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": DAY_TSS[1:5],
-            "top_1prc": [row[0] for row in DAY_VALS[1:5]],
-            "top_1k": [row[1] for row in DAY_VALS[1:5]],
-            "top_100": [row[2] for row in DAY_VALS[1:5]],
-            "top_10": [row[3] for row in DAY_VALS[1:5]],
-            "circ_supply": DAY_SCS[1:5],
-        }
-
-    def test_from_just_after_spot(self, client):
-        # from timestamp just after d4
-        # expect value at d5 and d6
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={DAY_TSS[3] + 1}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": DAY_TSS[4:6],
-            "top_1prc": [row[0] for row in DAY_VALS[4:6]],
-            "top_1k": [row[1] for row in DAY_VALS[4:6]],
-            "top_100": [row[2] for row in DAY_VALS[4:6]],
-            "top_10": [row[3] for row in DAY_VALS[4:6]],
-            "circ_supply": DAY_SCS[4:6],
-        }
-
-    def test_out_of_range(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={DAY_TSS[-1] + 10}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": [],
-            "top_1prc": [],
-            "top_1k": [],
-            "top_100": [],
-            "top_10": [],
-            "circ_supply": [],
-        }
-
-    def test_to_only(self, client):
-        # to timestamp of block 26
-        # expect values at d2, d3 and d4
-        url = base_url(client.address_type, self.r)
-        url += f"&to={DAY_TSS[3] + 100000}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": DAY_TSS[1:4],
-            "top_1prc": [row[0] for row in DAY_VALS[1:4]],
-            "top_1k": [row[1] for row in DAY_VALS[1:4]],
-            "top_100": [row[2] for row in DAY_VALS[1:4]],
-            "top_10": [row[3] for row in DAY_VALS[1:4]],
-            "circ_supply": DAY_SCS[1:4],
-        }
-
-    def test_from_prior_to_genesis(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={GENESIS_TIMESTAMP - 1}"
-        response = client.get(url)
-        assert response.status_code == 422
-
-    def test_from_ge_to(self, client):
-        url = base_url(client.address_type, self.r)
-        url += f"&fr={DAY_TSS[3]}&to={DAY_TSS[3] - 1}"
-        response = client.get(url)
-        assert response.status_code == 422
-        assert response.json()["detail"] == "Parameter `fr` cannot be higher than `to`"
-
-    def test_to_gt_sync_height(self, client):
-        # to 2 days after last one
-        # expect value at d5 and d6
-        url = base_url(client.address_type, self.r)
-        url += f"&to={DAY_TSS[-1] + DAY_DT * 2}"
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.json() == {
-            "timestamps": DAY_TSS[4:6],
-            "top_1prc": [row[0] for row in DAY_VALS[4:6]],
-            "top_1k": [row[1] for row in DAY_VALS[4:6]],
-            "top_100": [row[2] for row in DAY_VALS[4:6]],
-            "top_10": [row[3] for row in DAY_VALS[4:6]],
-            "circ_supply": DAY_SCS[4:6],
+            "timestamps": tss[4:6],
+            "top_1prc": [row[0] for row in vals[4:6]],
+            "top_1k": [row[1] for row in vals[4:6]],
+            "top_100": [row[2] for row in vals[4:6]],
+            "top_10": [row[3] for row in vals[4:6]],
+            "circ_supply": scs[4:6],
         }
 
 
