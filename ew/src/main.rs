b5d52f6f07548699bffa93786f53d43faeb52af9
chris <abc@abchris.xyz>
2023-07-09 08:47:59 +1200

    First executable using a dummy sink to consume core blocks

---
+++ ew/src/main.rs
@@ -0,0 +1,74 @@
+use std::env;
+use tokio;
+
+use ew::core::tracking::Tracker;
+use ew::core::types::Head;
+use ew::core::Node;
+use tracing::Instrument;
+
+/// Gives some time to tracing subscriber
+async fn sleep_some() {
+    let ms = 100;
+    tracing::info!("sleeping {ms}ms");
+    tokio::time::sleep(tokio::time::Duration::from_millis(ms)).await;
+}
+
+#[tokio::main]
+async fn main() -> Result<(), &'static str> {
+    // Configure tracing subscriber
+    let level = match env::var("EW_LOG_DEBUG") {
+        Ok(_) => tracing::Level::DEBUG,
+        _ => tracing::Level::INFO,
+    };
+    tracing::info!("found EW_POSTGRES_URI environment variable");
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_max_level(level)
+        .finish();
+    let _guard = tracing::subscriber::set_global_default(subscriber);
+
+    tracing::info!("starting EW");
+
+    // Env variables
+    let pg_uri = env::var("EW_POSTGRES_URI").unwrap();
+    tracing::info!("found EW_POSTGRES_URI environment variable");
+
+    let node_url = env::var("EW_NODE_URL").unwrap();
+    tracing::info!("found EW_NODE_URL environment variable");
+
+    tracing::info!("configuring tracker");
+    let node = Node::new("local-node", &node_url);
+    let pgconf = ew::config::PostgresConfig::new(&pg_uri);
+    let mut tracker = Tracker::new(node, pgconf).await;
+
+    // Dummy sink for now
+    let mut rx = tracker.add_cursor("main".to_string(), Head::initial());
+
+    // Start tracker
+    tokio::spawn(async move {
+        sleep_some().await;
+        tracker
+            .start()
+            .instrument(tracing::info_span!("tracker"))
+            .await;
+    });
+
+    // Dummy sink implementation
+    tracing::info!("starting dummy sink");
+    loop {
+        tokio::select! {
+            _ = tokio::signal::ctrl_c() => {break;},
+            msg = rx.recv() => {
+                let s = match msg.expect("message is some") {
+                    ew::core::tracking::TrackingMessage::Genesis(_) => String::from("genesis"),
+                    ew::core::tracking::TrackingMessage::Include(d) => format!("include {}", d.block.header.height),
+                    ew::core::tracking::TrackingMessage::Rollback(h) => format!("rollback {}", h),
+                };
+                tracing::debug!("dummy sink got message: {}", s);
+            },
+        }
+    }
+
+    tracing::info!("exiting");
+    Ok(())
+}
