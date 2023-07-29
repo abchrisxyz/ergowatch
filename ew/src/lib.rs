fcf8657f1d450a31ee3878512b15fe18031a25b1
chris <abc@abchris.xyz>
2023-07-29 12:17:25 +1200

    Drop dummy unit test

--- ew/src/lib.rs
+++ ew/src/lib.rs
@@ -2,13 +2,3 @@ pub mod config;
 pub mod core;
 pub mod utils;
 pub mod workers;
-
-#[cfg(test)]
-mod tests {
-    // use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
-    }
-}
